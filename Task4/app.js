2c597c473133a3234186b09e78b6b1c2637d2f17
volodymyrZotov <volodymyr.zotov@gmail.com>
2016-12-16 12:26:38 +0200

    add file README.md

--- Task4/app.js
+++ Task4/app.js
@@ -1,18 +1,1 @@
-/*This plagin can truncate text*/
-$.fn.trunc = function(number) {
-  var str = $(this).text();
-  var newStr = str.substr(0, number);
-
-  $(this).hide();
-  $(this).after('<p class="trunc"></p>');
-  $('.trunc').text(newStr);
-  $('.trunc').append('<span class="show">...</span>');
-
-  //hide truncated element and show old (full) element
-  $('.show').click(function () {
-    $(this).parent().hide();
-    $('.trunc').prev().show()
-  });
-}
-
 $('p:eq(1)').trunc(100);
---
+++ Task4/trunc.js
@@ -0,0 +1,16 @@
+/*This plagin can truncate text*/
+$.fn.trunc = function(number) {
+  var str = $(this).text();
+  var newStr = str.substr(0, number);
+
+  $(this).hide();
+  $(this).after('<p class="trunc"></p>');
+  $('.trunc').text(newStr);
+  $('.trunc').append('<span class="show">...</span>');
+
+  //hide truncated element and show old (full) element
+  $('.show').click(function () {
+    $(this).parent().hide();
+    $('.trunc').prev().show()
+  });
+}
